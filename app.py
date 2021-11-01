d0570187204486c2893f679af5709fea7d52aac9
woonys <wodns1324@naver.com>
2021-11-01 22:15:29 +0900

    init_db 작성

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
-#Hi, This is Jaewoon
+
+# mongodb://test:test@
+client = MongoClient('localhost', 27017)
+db = client.dbjungle
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
 
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+client = MongoClient('localhost', 27017)
+db = client.dbjungle
 
 if __name__ == '__main__':
     app.run()
