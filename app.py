9fbddb5f8c04457e352a9956e353cc30fc929ff1
Minsoo-Shin <alstn5038@gmail.com>
2021-11-02 21:33:13 +0900

    loggedin함수 및 login함수 구현

--- app.py
+++ app.py
@@ -1,72 +1,118 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
 from pymongo import MongoClient
 from bson.json_util import dumps
-from bson.objectid import ObjectId
-from init_db import insert_db
-
-app = Flask(__name__)
+from bson import ObjectId
+import werkzeug
+import bcrypt
 
 client = MongoClient('localhost', 27017)
 db = client.dbjungle
 
-@app.route('/', methods=['POST'])
+app = Flask(__name__)
+app.secret_key = 'any random string'
+
+@app.route('/logged_in')
+def logged_in():
+    if "id_receive" in session:
+        id = session["id_receive" ]
+        return render_template('logged_in.html', id = id)
+    else:
+        return redirect(url_for("login"))
+
+@app.route('/', methods = ['GET', 'POST'])
 def login():
-        #클라이언트로부터 데이터를 받기
-        id_receive = request.form['id']
-        pwd_receive = request.form['pwd']
-        try:
-            #member_db (db_mem_id, db_mem_pwd 열에서 회원 조회)
-            info_check = db.member_list.find_one({'user_id' : id_receive, 'user_pwd' : pwd_receive})
-            if info_check is not None:
-                print("회원체크 완료")
-                return jsonify({'result': 'success', 'name': id_receive})
-            else:
-                print("아이디/비번 재확인")
-                return jsonify({'result': 'fail', 'msg':'아이디/비밀번호 재확인해주세요.'})
-        except:
-            return jsonify({'result': 'fail', 'msg':'except.'})
-
-@app.route('/memo', methods=['GET'])
-def show_memo():
-    # 1. mongoDB에서 _id 값 포함한 모든 데이터 조회해오기 (Read)
-    result = list(db.memos.find({}))
-    # 2. articles라는 키 값으로 article 정보 보내주기
-    return jsonify({'result': 'success', 'articles': dumps(result)})
-
-
-@app.route('/memo', methods=['POST'])
-def post_article():
-    # 1. 클라이언트로부터 데이터를 받기
-    week_receive = request.form['week']
-    title_receive = request.form['title']  # 클라이언트로부터 title을 받는 부분
-    content_receive = request.form['content']  # 클라이언트로부터 comment를 받는 부분
-
-    memo = {'week' : week_receive, 'title': title_receive, 'content': content_receive}
-
-    # 3. mongoDB에 데이터를 넣기
-    db.member_list.insert_one(memo)
-    return jsonify({'result': 'success'})
-   
-
-@app.route('/api/delete', methods=['POST'])
-def del_article():
-    # 들어온 이름으로 DB에 name값의 data를 삭제한다. 
-    del_id_receive = request.form['del_id']
-
-    db.articles.delete_one({'_id': ObjectId(del_id_receive)})
+    message = 'Please login to your account'
+    if 'user_id' in session:
+        return redirect(url_for("logged_in"))
     
-    return jsonify({'result': 'success'})
+    if request.method == 'GET':
+        return render_template("login.html")
 
-@app.route('/api/update', methods=['POST'])
-def update_article():
-    update_id_receive = request.form['update_id']
-    update_title_receive = request.form['update_title']
-    update_comment_receive = request.form['update_comment']
+    if request.method == 'POST':
+        #클라이언트로부터 데이터를 받기
+        id_receive = request.form['id_give']
+        pwd_receive = request.form['pwd_give']
+        #print("id, pwd 받음", id_receive, pwd_receive)
+        
+        #member_list DB에서 사용자 조회 (db_mem_id, db_mem_pwd 열에서 회원 조회)
+        info_check = db.member_list.find_one({ 'user_id' : id_receive})
+        if info_check is not None:
+            print("회원체크 완료")
+            id_val = info_check['user_id'] #DB ID 값
+            passwordcheck = info_check['pwd'] #DB PWD 값
 
-    db.articles.update_one({'_id': ObjectId(update_id_receive)},{'$set':{'title':update_title_receive, 'comment': update_comment_receive}})
+            if bcrypt.checkpw(pwd_receive.encode('utf-8'), passwordcheck): #DB PWD/입력값 비교
+                session['user_id'] = id_val #이름과 user_id로 세션
+                return redirect(url_for('logged_in'))
 
-    return jsonify({'result': 'success'})
+            else:
+                # if session['id_receive'] in session:
+                #     return redirect(url_for("logged_in"))
+                message = 'Wrong password'
+                return render_template('/', message=message)
 
+        else:
+            message = 'Email not found'
+            return render_template('/', message=message)
+    return render_template('/', message=message)
+
+####################
+#여기서부터 재운
+#2. main page
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+#세션에서 이름, 전화번호(식별자)를 넘겨받기
+
+
+@app.route('/memos', methods=['POST'])
+def create_card():
+    name = session['name']
+    phone = session['phone']
+    week_receive = request.form['week_give']
+    title_receive = request.form['title_give']
+    content_receive = request.form['content_give']
+
+    doc = {'name':name, 'phone': phone, 'title': title_receive, 'content': content_receive, 'week': week_receive}
+
+    db.member_writing.insert_one(doc)
+
+    return jsonify({'result': 'success', 'msg': '오늘 하루도 수고많으셨어요!'})
+
+@app.route('/memos', methods=['GET'])
+def list_my_cards():
+    week_receive = request.form['week_give']
+    all_mywrts = list(db.member_writing.find({'phone': session['phone'], 'week': week_receive}))
+    for wrt in all_mywrts:
+        wrt['id'] = str(wrt.pop('_id'))
+    return jsonify({'result': 'success', 'all_writing': all_mywrts})
+
+@app.route('/memos', methods=['GET'])
+def list_others_cards():
+    week_receive = request.form['week_give']
+    all_othwrt = list(db.member_writing.find({'week': week_receive}))
+    for write in all_othwrt:
+        if write['phone'] == session['phone']:
+            all_othwrt.remove(write)
+    return jsonify({'result': 'success', 'all_writing': all_othwrt})
+
+
+@app.route('/memos', methods=['PATCH'])  # vs PUT
+def update_card():
+    id_receive = ObjectId(request.form['id_give']) #string
+    title_receive = request.form['title_give']
+    content_receive = request.form['contents_give']
+    week_receive = request.form['week_give']
+
+    db.exam.update_one({'_id': id_receive}, {'$set': {'title': title_receive, 'content': content_receive, 'week': week_receive}})
+    return jsonify({'result': 'success', 'msg': '수정 완료'})
+
+@app.route('/memos/{id:str}', methods=['DELETE'])
+def delete_card():
+    id_receive = ObjectId(request.form['id_give'])
+    db.member_writing.delete_one({'_id': id_receive})
+    return jsonify({'result': 'success', 'msg': '제거되었습니다!'})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run()
