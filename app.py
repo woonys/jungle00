65fdcdac8d997380ec3436a9d58047de946d13ed
이지연 <ijiyeon@ijiyeon-ui-MacBookPro.local>
2021-11-03 09:39:12 +0900

    클라이언트 작업 완료

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from flask import Flask ,jsonify, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template("join.html")
+
+
+@app.route('/login', methods=['POST'])
+def create_card():
+    return jsonify({'result': 'fail', 'msg': 'asdasd'})
 
 
 if __name__ == '__main__':
